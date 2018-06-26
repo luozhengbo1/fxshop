8eddda916ba6eedc68cbb9f8fa981c9f0f842759
liang fan <liangfan_smile@163.com>
2018-06-26 09:31:56 +0800

    会员管理完成

--- application/admin/controller/Customer.php
+++ application/admin/controller/Customer.php
@@ -27,7 +27,7 @@ class Customer extends Controller
 
         //按会员等级搜索
         if ($this->request->param("grade")) {
-            $map['grade'] = ["like", $this->request->param("grade")];
+            $map['grade_id'] = $this->request->param("grade");
         }
 
         //按创建时间段搜索
@@ -51,7 +51,7 @@ class Customer extends Controller
     public function excel()
     {
         if ($this->request->isPost()) {
-            $header = ['用户名', '昵称', '性别', '生日', '手机号', '邮箱', '积分','等级', '头像','微信ID', '登录IP', '创建时间'];
+            $header = ['用户名', '昵称', '性别', '生日', '手机号', '邮箱', '积分', '等级', '头像', '微信ID', '登录IP', '创建时间'];
             $data = \think\Db::name("customer")->field("username,nickname,sex,birthday,mobile,email,score,grade,headimgurl,openid,login_ip,create_time")->order("id desc")->select();
             if ($error = \Excel::export($header, $data, "会员信息表", '2007')) {
                 throw new Exception($error);
@@ -101,20 +101,35 @@ class Customer extends Controller
         if (method_exists($this, 'filter')) {
             $this->filter($map);
         }
-        $this->datalist($model, $map);
-        return $this->view->fetch();
-    }
 
-    public function getGradeVal()
-    {
-        $data = \app\common\model\CustomerGrade::all();
-        for ($i = 0; $i < count($data); $i++) {
-            $item = $data[$i]->toArray();
-            dump($item);
-            $id[$i] = $item['id'];
-            $name[$i] = $item['name'];
+        //  根据积分自动更新grade
+        $score = $model->where('isdelete', '0')->select();
+        $where = "1=1";
+        $typeList = Model('CustomerGrade')->field('id ,name,score_start,score_end')->order('id ')->where($where)->select();
+        for ($i = 0; $i < count($score); $i++) {
+            $scoreitem = $score[$i];
+            for ($j = 0; $j < count($typeList); $j++) {
+                $listitem = $typeList[$j];
+                if ($scoreitem['score'] > $listitem['score_start'] && $scoreitem['score'] < $listitem['score_end']) {
+                    //插入grade_id
+                    $model->save([
+                        'grade_id' => $listitem['id']
+                    ], ['id' => $scoreitem['id']]);
+                    $gradelist[$i]=[
+                        'name'=>$listitem['name']
+                    ];
+                }
+            }
         }
-        dump($id);
-        dump($name);
+        //根据grade_id查询grade名称
+//        $list = $model->alias('customer')
+//            ->field('customer.*,fy_customer_grade.name')
+//            ->join('fy_customer_grade', 'customer.grade_id=fy_customer_grade.id', 'left')
+//            ->where('customer.isdelete','0')
+//            ->select();
+        $this->datalist($model, $map);
+        $this->view->assign('typelist', $typeList);
+//        $this->view->assign('gradelist', $gradelist);
+        return $this->view->fetch();
     }
 }
