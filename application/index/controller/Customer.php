1b9ee723dc3e307a2b28a0282bc5f65483c966db
liang fan <liangfan_smile@163.com>
2018-07-05 14:19:34 +0800

    收藏夹和签到接口修改

--- application/index/controller/Customer.php
+++ application/index/controller/Customer.php
@@ -11,16 +11,14 @@ use think\Db;
 class Customer extends Controller
 {
     /**
-     * 收藏夹
+     * 收藏夹列表
      */
-    public function collect_list()
+    public function collect_list($page = '1', $size = '10')
     {
-        //获取前端传过来的分页数据
+        $this->assign('titleName', '收藏夹');
         if ($this->request->isAjax()) {
+            //查询用户id
             $user = session('wx_user');
-            $data = $this->request->post();
-            $page = $data['page'] * $data['size'] - 4;
-            $size = $data['size'];
             $customer = Db::table('fy_customer')->field('id')->where('openid', $user['openid'])->find();
             $uid = $customer['id'];
             //根据customer.id=collect.uid查询用户收藏的所有商品信息（goods.id=collect.goods_id）
@@ -29,10 +27,17 @@ class Customer extends Controller
                 ->join('fy_customer customer', "collect.uid = customer.id  and customer.id=$uid")
                 ->join('fy_goods goods', "collect.goods_id = goods.id")
                 ->field('collect.uid,goods.id,goods.name,goods.main_image,goods.status,goods.price')
-                ->limit($page, $size)
+                ->page($page, $size)
                 ->where('collect.status', '1')
                 ->select();
-            $this->view->assign('list_collect', $list_collect);
+            //判断用户收藏列表是否为空
+            if ($list_collect) {
+                $this->view->assign('list_collect', $list_collect);
+                return ajax_return($list_collect, 'OK', 200);
+            } else {
+                return ajax_return('', '您还没有收藏商品哦', 203);
+            }
+        } else {
             return $this->view->fetch('goodscollect');
         }
     }
@@ -44,32 +49,26 @@ class Customer extends Controller
     {
         if ($this->request->isAjax()) {
             $user = session('wx_user');
-            $data = $this->request->post();
             $customer = Db::table('fy_customer')->where('openid', $user['openid'])->find();
+            $uid = $customer['id'];
+            $data = $this->request->post();
+            $goods_id = $data['id'];
+            if (!$goods_id) {
+                return $this->error('缺少参数id');
+            }
             //取消指定用户的指定收藏内容
-            Db::table('fy_customer_collect')->where('goods_id', $data['id'])->where('uid', $customer['id'])->setField('status', '0');
+            Db::table('fy_customer_collect')->where('uid', $uid)->where('goods_id', $goods_id)->setField('status', '0');
+            return ajax_return('', '取消收藏成功', 200);
         }
     }
-
-    /**
-     * 签到主页面
-     */
-//    public function sign_main()
-//    {
-//        $user = session('wx_user');
-//        //根据openid在customer表中查询id,nickname,score,continuity_day
-//        $userData = Db::table('fy_customer')->where('openid', $user['openid'])->find();
-//        $this->view->assign('userData', $userData);
-//        return $this->view->fetch('mysign');
-//    }
 
     /**
      * 签到
      */
     public function my_sign()
     {
-        $user = session('wx_user');
         //根据openid在customer表中查询id,nickname,score,continuity_day
+        $user = session('wx_user');
         $userData = Db::table('fy_customer')->where('openid', $user['openid'])->find();
         if ($this->request->get()) {
             $time = time();
@@ -83,12 +82,6 @@ class Customer extends Controller
             $this->assign('user', $userData);
             return $this->view->fetch('mysign');
         } else if ($this->request->isAjax()) {
-//            签到规则
-//             1、每天只能签到一次。
-//             2、连续签到3天，一次性奖励2积分。
-//             3、连续签到7天，一次性奖励8积分。
-//             4、连续签到15天，一次性奖励20积分。
-//             5、连续签到30天，一次性奖励50积分。
             $time = time();
             $today_start = strtotime(date('Y-m-d', $time) . ' 00:00:00');
             $today_end = strtotime(date('Y-m-d', $time) . ' 23:59:59');
@@ -98,7 +91,7 @@ class Customer extends Controller
                 ->where('addtime', 'between', [$today_start, $today_end])
                 ->find();
             if ($res) {
-                $this->ajaxReturn(['status' => 501, 'msg' => '今天已经签过了']);
+                return ajax_return('', '今天已经签过了', 204);
                 die;
             } else {
                 $save = [
@@ -114,7 +107,7 @@ class Customer extends Controller
                     Db::table('fy_customer')->where('openid', $user['openid'])->setField('continuity_day', 1);
                     Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', 1);
                     Db::table('fy_customer_sign')->insert($save);
-                    $this->ajaxReturn(['status' => 200, 'msg' => '签到成功']);
+                    return ajax_return('', '签到成功', 200);
                     exit;
                 } else {
                     //判断前一天是否签到
@@ -126,10 +119,17 @@ class Customer extends Controller
                         Db::table('fy_customer')->where('openid', $user['openid'])->setField('continuity_day', 1);
                         Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', 1);
                         Db::table('fy_customer_sign')->insert($save);
-                        $this->ajaxReturn(['status' => 201, 'msg' => '签到成功']);
+                        return ajax_return('', '签到成功', 200);
                     } else {
-                        //判断今天签到后是否满足奖励条件
+                        /**
+                         * 签到规则
+                         * 连续签到3天，一次性奖励2积分。
+                         * 连续签到7天，一次性奖励8积分。
+                         * 连续签到15天，一次性奖励20积分。
+                         * 连续签到30天，一次性奖励50积分。
+                         * */
                         $score = 0;
+                        //判断今天签到后是否满足奖励条件
                         switch ($userData['continuity_day']) {
                             case '2':
                                 $score = 2;
@@ -148,26 +148,24 @@ class Customer extends Controller
                         if ($score) {
                             Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', $score + 1);
                             Db::table('fy_customer')->where('openid', $user['openid'])->setInc('continuity_day', 1);
-                            $save = [
+                            $newSave = [
                                 'addtime' => $time,
                                 'uid' => $userData['id'],
                                 'score' => 1,
                                 'reward_score' => $score
                             ];
-                            Db::table('fy_customer_sign')->insert($save);
+                            Db::table('fy_customer_sign')->insert($newSave);
                         } else {
-                            //是否已达到最大连续签到天数？continue_day重置为1，score+1更新到customer表
-                            if ($userData['continue_day'] = 30) {
+                            //是否已达到最大连续签到天数？continue_day重置为1
+                            if ($userData['continuity_day'] == 30) {
                                 Db::table('fy_customer')->where('openid', $user['openid'])->setField('continuity_day', 1);
-                                Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', 1);
-                                Db::table('fy_customer_sign')->insert($save);
                             } else {
-                                Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', 1);
                                 Db::table('fy_customer')->where('openid', $user['openid'])->setInc('continuity_day', 1);
-                                Db::table('fy_customer_sign')->insert($save);
                             }
+                            Db::table('fy_customer')->where('openid', $user['openid'])->setInc('score', 1);
+                            Db::table('fy_customer_sign')->insert($save);
                         }
-                        $this->ajaxReturn(['status' => 202, 'msg' => '签到成功']);
+                        return ajax_return('', '签到成功', 200);
                     }
                 }
             }
