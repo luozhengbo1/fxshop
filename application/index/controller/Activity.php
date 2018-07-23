dd5f5d33f1cc7e339fabc39d6e2b5f430aac8546
jiangyongfei <1299625459@qq.com>
2018-07-23 17:15:03 +0800

    限时抢购完善

--- application/index/controller/Activity.php
+++ application/index/controller/Activity.php
@@ -20,7 +20,7 @@ Class Activity extends Mustlogin
             $size = $this->request->param('size');
             $activity = Db::name('activity')
                 ->where(['isdelete'=>0,'status'=>1])
-                ->order('fy_activity.create_time desc')
+                ->order('fy_activity.end_date desc')
                 ->page($page,$size)
                 ->select();
             return ajax_return($activity,'','200');
