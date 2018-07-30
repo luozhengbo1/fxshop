8dff25e21b11bca1e583fae08fe71eea03212be2
guoliang <guoliang@qq.com>
2018-07-30 15:45:45 +0800

    bug地址删除之后，订单地址不在显示bug

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -94,7 +94,7 @@ class Order extends Controller
 
         $userList = Db::name('admin_user')->where(['isdelete'=>0,'id'=>['>',1]])->select();
         $orderList = Db::name('order')
-           ->field('fy_order.id,fy_order.buy_list,fy_order.address_id,fy_order.pay_status,
+           ->field('fy_order.id,fy_order.buy_list,fy_order.address_id,fy_order.pay_status,fy_order.address_detail,
             fy_order.order_id,fy_order.order_status,fy_order.total_price,fy_order.customer_name,fy_order.customer_name,fy_order.create_time,fy_order.pay_time,
             fy_order_goods.user_id,fy_order_goods.is_send, fy_order_goods.is_send,fy_order_goods.is_return')
             ->join( 'fy_order_goods','fy_order_goods.order_id=fy_order.order_id','left')
