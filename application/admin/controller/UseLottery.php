4b3ba63d8c7b0dfa5dd43d3322f399924da7bcf0
guoliang <guoliang@qq.com>
2018-08-10 11:32:41 +0800

    后端核销记录

--- application/admin/controller/UseLottery.php
+++ application/admin/controller/UseLottery.php
@@ -11,20 +11,17 @@ class UseLottery extends Controller
     use \app\admin\traits\controller\Controller;
     // 方法黑名单
     protected static $blacklist = [];
-    protected $beforeActionList = [
-        'beforeIndex' =>  ['except'=>'index'],
-    ];
 
     protected function filter(&$map)
     {
         if ($this->request->param("username")) {
-            $map['fy_use_lottery.username'] = ["like", "%" . $this->request->param("username") . "%"];
+            $map['username'] = ["like", "%" . $this->request->param("username") . "%"];
         }
         if ($this->request->param("shop_openid")) {
-            $map['fy_use_lottery.shop_openid'] = ["like", "%" . $this->request->param("shop_openid") . "%"];
+            $map['shop_openid'] = ["like", "%" . $this->request->param("shop_openid") . "%"];
         }
         if ($this->request->param("lottery_id")) {
-            $map['fy_use_lottery.lottery_id'] = $this->request->param("lottery_id");
+            $map['lottery_id'] = $this->request->param("lottery_id");
         }
 
     }
@@ -32,10 +29,13 @@ class UseLottery extends Controller
     public function beforeIndex()
     {
         $user = Db::name('admin_user')->where(['openid'=>['<>','']])->select();
-        #
         $lottery = Db::name('lottery')->where(['isdelete'=>0,'status'=>1,'type'=>3])->select();
-        dump($lottery);die;
         $this->view->assign('adminUser',$user);
         $this->view->assign('lottery',$lottery);
     }
+    public function beforeRecyclebin()
+    {
+        $this->beforeIndex();
+    }
+
 }
