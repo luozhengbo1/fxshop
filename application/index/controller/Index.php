68cd765bd94283203ea5f47ebe9a8329a9d89d34
guoliang <guoliang@qq.com>
2018-08-15 14:41:21 +0800

    ‘礼包接口’

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -25,7 +25,6 @@ class Index extends Mustlogin
         $modular = new Modular($num = 5);
         $getModular = $modular->getModular();
         $this->view->assign('modular', $getModular);
-        $this->view->assign('lotteryList', $lotteryList);
         $this->view->assign('titleName', "泛亚商城");
         return $this->fetch();
     }
@@ -36,10 +35,10 @@ class Index extends Mustlogin
         $this->assign('titleName', "个人消息 ");
         return $this->view->fetch();
     }
-    public function newCustomerGiftBag()
+    public function newCustomerGiftBag($gift_bag_id=3)
     {
         #查询是否有新人礼包
-        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'status'=>0,'gift_bag_id'=>3])->find();
+        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'status'=>0,'gift_bag_id'=>$gift_bag_id])->find();
         $lotteryList=[];
         if($giftBagLog){#未发放
             $giftBag = Db::name('gift_bag')->where(['id'=>$giftBagLog['gift_bag_id']])->find();
@@ -50,25 +49,12 @@ class Index extends Mustlogin
                 }
             }
         }
-        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'gift_bag_id'=>3])->update(['status'=>1]);
+        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'gift_bag_id'=>$gift_bag_id])->update(['status'=>1]);
         return ajax_return($lotteryList,'ok',200);
     }
-    public function birthdayCustomerGiftBag()
+    public function birthdayCustomerGiftBag($gift_bag_id=2)
     {
-        #查询是否有新人礼包
-        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'status'=>0,'gift_bag_id'=>2])->find();
-        $lotteryList=[];
-        if($giftBagLog){#未发放
-            $giftBag = Db::name('gift_bag')->where(['id'=>$giftBagLog['gift_bag_id']])->find();
-            if( trim($giftBag['lottery_id']) ){
-                $lotteryId = explode(',', trim($giftBag['lottery_id']));
-                foreach ($lotteryId as $vId){
-                    $lotteryList[] = Db::name('lottery')->where(['id'=>$vId])->find();
-                }
-            }
-        }
-        $giftBagLog = Db::name('gift_bag_log')->where(['openid'=> $this->userInfo['openid'],'gift_bag_id'=>2])->update(['status'=>1]);
-        return ajax_return($lotteryList,'ok',200);
+        return $this->newCustomerGiftBag(2);
     }
     public function demo()
     {
